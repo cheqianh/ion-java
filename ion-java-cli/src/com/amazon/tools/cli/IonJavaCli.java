8a26c0e124b4bec6821a62c0ec6c5da7cc590db5
Eric Chen <cheqianh@amazon.com>
2020-09-08 19:38:35 -0700

    handle /bin/zsh annotations from eventstream

--- ion-java-cli/src/com/amazon/tools/cli/IonJavaCli.java
+++ ion-java-cli/src/com/amazon/tools/cli/IonJavaCli.java
@@ -1070,12 +1070,12 @@ public class IonJavaCli {
                     ionReader.stepIn();
                     while (ionReader.next() != null) {
                         ionReader.stepIn();
-                        String text = "";
+                        String text = null;
                         int sid = 0;
                         while (ionReader.next() != null) {
                             switch (ionReader.getFieldName()) {
                                 case "text":
-                                    text = ionReader.stringValue();
+                                    text = ionReader.isNullValue() ? null : ionReader.stringValue();
                                     break;
                                 case "sid":
                                     sid = ionReader.intValue();
@@ -1086,7 +1086,7 @@ public class IonJavaCli {
                         annotationsList.add(annotation);
                         ionReader.stepOut();
                     }
-                    annotations = annotationsList.toArray(new SymbolToken[0]);
+                    annotations = annotationsList.toArray(SymbolToken.EMPTY_ARRAY);
                     ionReader.stepOut();
                     break;
                 case "value_text":
