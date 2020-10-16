8cb8b6351a00ecbaa99be348ff588c5c075eaac0
Eric Chen <cheqianh@amazon.com>
2020-10-16 14:45:20 -0700

    update new extension.
    change back errors.

--- ion-java-cli/src/com/amazon/tools/cli/IonJavaCli.java
+++ ion-java-cli/src/com/amazon/tools/cli/IonJavaCli.java
@@ -232,8 +232,7 @@ public class IonJavaCli {
             processContext.setLastEventType(event.getEventType());
             if (event.getEventType() == EventType.CONTAINER_START) {
                 if (isEmbeddedEvent(event)) {
-//                    count = embeddedEventToIon(processContext, args, count, event.getIonType());
-                    embeddedEventToIon(processContext, args, count, event.getIonType());
+                    count = embeddedEventToIon(processContext, args, count, event.getIonType());
                 } else {
                     IonType type = event.getIonType();
                     setFieldName(event, processContext.getIonWriter());
